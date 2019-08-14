37624a6fdca3e3acb07ee8fa9b7acab8ac75c2a4
Nalen98 <52778977+Nalen98@users.noreply.github.com>
2019-08-14 18:02:46 +0300

    Add files via upload

---
+++ src/main/java/ghidra/app/util/bin/format/elf/extend/eBPF_ElfExtension.java
@@ -0,0 +1,38 @@
+
+package ghidra.app.util.bin.format.elf.extend;
+
+import ghidra.app.util.bin.format.elf.*;
+import ghidra.program.model.lang.*;
+import ghidra.util.exception.*;
+import ghidra.util.task.TaskMonitor;
+
+public class eBPF_ElfExtension extends ElfExtension {
+	
+	@Override
+	public boolean canHandle(ElfHeader elf) {
+		return elf.e_machine() == ElfConstants.EM_BPF && elf.is64Bit();
+	}	
+	@Override
+	
+	public boolean canHandle(ElfLoadHelper elfLoadHelper) {
+		Language language = elfLoadHelper.getProgram().getLanguage();
+		return canHandle(elfLoadHelper.getElfHeader()) && 
+				"eBPF".equals(language.getProcessor().toString()) &&
+						language.getLanguageDescription().getSize() == 64;
+	}
+
+	@Override
+	public String getDataTypeSuffix() {
+		return "eBPF";
+	}
+
+	@Override
+	public void processGotPlt(ElfLoadHelper elfLoadHelper, TaskMonitor monitor) throws CancelledException {
+		
+		if (!canHandle(elfLoadHelper)) {
+			return;
+		}		
+		super.processGotPlt(elfLoadHelper, monitor);
+	}	
+
+}
